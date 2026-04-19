e479b6b3f72106407e650f2a2b2fba8b5209f010
singhrishu91109-coder <singhrishu91109@gmail.com>
2026-04-19 15:58:18 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,3 +28,22 @@ train_df["Embarked"] = train_df["Embarked"].fillna(train_df["Embarked"].mode()[0
 test_df["Embarked"] = test_df["Embarked"].fillna(test_df["Embarked"].mode()[0])
 
 test_df["Fare"] = test_df["Fare"].fillna(np.median(test_df["Fare"]))
+
+# ================================
+# Step 3 : Encoding
+# ================================
+train_df = pd.get_dummies(train_pdf,drop_first=True)
+test_df = pd.get_dummies(test_df,drop_first=True)
+
+#Align columns
+test_df = test_df.reindex(columns = train_df.drop("Survived",axis=1).columns, fill_value=0)
+
+# ================================
+# step 4: Define X and Y
+# ================================
+X = train_df.drop("Survived",axis=1)
+Y = train_df["Survived"]
+
+# Convert to Numpy arrays(explict use)
+X = np.array(X)
+Y = np.array(Y)
